0968bf792c336da830088e4d9b233fe25bfcc6e4
LogashreeDeepak <logashreesureshkumar@gmail.com>
2026-09-23 17:15:24 +0530

    new changes

--- app.py
+++ app.py
@@ -8,4 +8,12 @@ git Add -A - adition, modification and deletion accrosss the repository
 git branch
 git switch
 git switch -c feature-login - create and switch to the new branch 
-
+git branch -d - safe deletion
+git branch -D - force deletion
+branch naming conventions - feature/user-authentication, feature/payment-api,bugfix/login-error
+hotfix/payment-production,release/v2.1.0
+merge conflict - A conflict occurs when Git cannot automatically determine how to combine changes.
+git merge --abort - Suppose you're in the middle of a complicated merge and decide you don't want to continue.
+origin - when you clone a remote repo git normally givea the remote repo this name origin
+git fetch - downloads the latest objects and references from the remote repository without merging those changes into my current local branch.
+git pull - git fetch + git merge
