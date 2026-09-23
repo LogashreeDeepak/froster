aea1ffc6d5641a1749780df7346dcf8114deb7df
LogashreeDeepak <logashreesureshkumar@gmail.com>
2026-09-23 16:39:40 +0530

    updated the changes

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 git commit --amend -m "Correct commit message"
+git diff --staged / --cached - to see the staged changes
+git restore --staged database.py - accidently staged the changes by git add database.py but u dont want it to be staged
+git show <commitid> - This shows the commit information and its changes.
+git rm old-config.yaml - remove files from the working directory and stages the deletion
+git mv old-name.txt new-name.txt - rename /move File     
+git Add -A - adition, modification and deletion accrosss the repository
+git branch
+git switch
+git switch -c feature-login - create and switch to the new branch 
 
